1612a2fce0bd7ee50867c222206d091bb3273556
Nikita <belka.nik02@gmail.com>
2025-03-03 17:44:59 +0200

    backup too

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 // firebase.js
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { getAuth, setPersistence, browserLocalPersistence } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDDNQg46t_eyi5vaaGf97kQPHI0qz2D8j4',
@@ -16,14 +16,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
-
-// Настраиваем персистентность сессии (локальное хранилище)
-setPersistence(auth, browserLocalPersistence)
-  .then(() => {
-    console.log('Персистентность сессии настроена на локальное хранилище');
-  })
-  .catch((error) => {
-    console.error('Ошибка настройки персистентности:', error);
-  });
 
 export { auth, db };
