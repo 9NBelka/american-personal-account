a5fbaad7b8f5af139d99143a9fa2f7a95a7b35bf
Nikita <belka.nik02@gmail.com>
2025-04-08 13:45:34 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,9 @@ import './main.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
 );
